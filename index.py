965d25a9b40d7bf899e43ab84f8e29f3d2d99e7c
SansUndertaleLab <70209030+SansUndertaleLab@users.noreply.github.com>
2022-05-06 13:31:14 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,15 @@
 from mkweb import *
 app=WebApp("index.html")
+app.settitle("mkweb test")
 app.setDoctype("html")
-app.addelement("body","test")
+app.addelement("body","This is a test website made with mkweb. It is currently not usable, however, it will soon be usable. Thank you.")
+app.addelement("p","this is a class test",["red-text"])
+app.addelement("style","""
+body{
+    background-color: lightblue;
+}
+.red-text{
+    color: red
+}
+""")
 app.create()
